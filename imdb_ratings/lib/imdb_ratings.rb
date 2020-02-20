32ea4fd2f6f1e5858a69bbf9edf27bfba37f5f5d
Chris Lincoln <chris.lincoln12@gmail.com>
2020-02-20 22:47:33 +0000

    First commit

---
+++ imdb_ratings/lib/imdb_ratings.rb
@@ -0,0 +1,9 @@
+require_relative "imdb_ratings/version"
+require_relative "imdb_ratings/cli"
+
+require 'pry'
+
+module ImdbRatings
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ imdb_ratings/lib/imdb_ratings/cli.rb
@@ -0,0 +1,35 @@
+class ImdbRatings::CLI
+  def call
+    puts "\nWelcome to IMDB Ratings\n"
+    get_genres
+    list_genres
+    get_user_genre
+  end
+  
+  def get_genres
+    #get_genres: eventually scrape ONLY the movie genres off of IMDB
+    @genres = ['Action', 'Animation', 'Comedy', 'Sci-Fi']
+  end
+  
+  def list_genres
+    #list_genres: list the scraped genres in the form of a string
+    @genres.each.with_index(1) do |genre, index|
+      puts "#{index}. #{genre}"
+    end
+  end
+  
+  def get_user_genre
+    #get_user_genre: get and validate the users selected genre via numeric input
+    chosen_genre = gets.strip.to_i
+    show_genres(chosen_genre) if valid_input?(chosen_genre, @months)
+  end
+  
+  def valid_input?(input, data)
+    #valid_input?: Does the user input fall between the range of listed genres?
+    input.to_i <= data.length && input.to_i > 0
+  end
+  
+  def show_genres(chosen_genre)
+    genre = @genres[chosen_genre - 1]
+  end
+end
